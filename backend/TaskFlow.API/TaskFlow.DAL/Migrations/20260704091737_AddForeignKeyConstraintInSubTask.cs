05e3962a63b6a1cbb3b1ea2457f970fa5618fe36
abithan0421 <abitha.n0421@gmail.com>
2026-07-04 15:01:35 +0530

    [fix]-Add foreign key constraint in subtask

---
+++ backend/TaskFlow.API/TaskFlow.DAL/Migrations/20260704091737_AddForeignKeyConstraintInSubTask.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyConstraintInSubTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""SubTaskItems""
+                ADD CONSTRAINT ""FK_SubTaskItems_TaskItems_MainTaskId""
+                FOREIGN KEY (""MainTaskId"")
+                REFERENCES ""TaskItems""(""Id"")
+                ON DELETE CASCADE;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""SubTaskItems""
+                DROP CONSTRAINT ""FK_SubTaskItems_TaskItems_MainTaskId"";
+            ");
+        }
+    }
+}
